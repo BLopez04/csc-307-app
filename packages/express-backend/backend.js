4fa1d095e622f42f0185eca14e481686ebf93532
BLopez04 <bernardo41504@gmail.com>
2024-10-08 16:28:52 -0700

    - Added users/:id GET endpoint

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -33,17 +33,21 @@ const users = {
     ]
 };
 
+const findUserByName = (name) => {
+    return users["users_list"].filter(      // filter since this is for a query
+        (user) => user["name"] === name
+    );
+};
+
+const findUserById = (id) =>           // find since this is for a specific one
+    users["users_list"].find((user) => user["id"] === id);
+
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
-
-const findUserByName = (name) => {
-    return users["users_list"].filter(
-        (user) => user["name"] === name
-    );
-};
 
 app.get("/users", (req, res) => {
     const name = req.query.name;
@@ -53,6 +57,16 @@ app.get("/users", (req, res) => {
         res.send(result);
     } else {
         res.send(users);
+    }
+});
+
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send("Resource not found.");
+    } else {
+        res.send(result);
     }
 });
 
